70ab923cafd75715ce072f44f08dcf6944542d6e
ArielGreiner <ariel.kiara@gmail.com>
2016-01-19 18:44:24 -0500

    Added in sum'd up data frame and a sample plot, removed independentswap code because it wasn't working.  Trial swap is not showing any results and frequency doesn't show anything for the regional trials

--- run_nullmodeltest.R
+++ run_nullmodeltest.R
@@ -55,9 +55,9 @@ Data_storage$sesMPDsp_abund_z[Data_storage$Dispersal==DispV[i] & Data_storage$Re
 
 Data_storage$sesMNTDsp_abund_z[Data_storage$Dispersal==DispV[i] & Data_storage$ReplicateNum==j & Data_storage$Scale == "Local"]<-mean(ses.mntd(samp = com_data,dis = interspec_mat,null.model = "sample.pool",abundance.weighted = T, runs = 999)$mntd.obs.z)
 
-Data_storage$sesMPDis_abund_z[Data_storage$Dispersal==DispV[i] & Data_storage$ReplicateNum==j & Data_storage$Scale == "Local"]<-mean(ses.mpd(samp = com_data,dis = interspec_mat,null.model = "independentswap",abundance.weighted = T, runs = 999)$mpd.obs.z)
+#Data_storage$sesMPDis_abund_z[Data_storage$Dispersal==DispV[i] & Data_storage$ReplicateNum==j & Data_storage$Scale == "Local"]<-mean(ses.mpd(samp = com_data,dis = interspec_mat,null.model = "independentswap",abundance.weighted = T, runs = 999)$mpd.obs.z)
 
-Data_storage$sesMNTDis_abund_z[Data_storage$Dispersal==DispV[i] & Data_storage$ReplicateNum==j & Data_storage$Scale == "Local"]<-mean(ses.mntd(samp = com_data,dis = interspec_mat,null.model = "independentswap",abundance.weighted = T, runs = 999)$mntd.obs.z)
+#Data_storage$sesMNTDis_abund_z[Data_storage$Dispersal==DispV[i] & Data_storage$ReplicateNum==j & Data_storage$Scale == "Local"]<-mean(ses.mntd(samp = com_data,dis = interspec_mat,null.model = "independentswap",abundance.weighted = T, runs = 999)$mntd.obs.z)
 
 Data_storage$sesMPDts_abund_z[Data_storage$Dispersal==DispV[i] & Data_storage$ReplicateNum==j & Data_storage$Scale == "Local"]<-mean(ses.mpd(samp = com_data,dis = interspec_mat,null.model = "trialswap",abundance.weighted = T, runs = 999)$mpd.obs.z)
 
@@ -95,9 +95,9 @@ Data_storage$sesMPDsp_abund_z[Data_storage$Dispersal==DispV[i] & Data_storage$Re
 
 Data_storage$sesMNTDsp_abund_z[Data_storage$Dispersal==DispV[i] & Data_storage$ReplicateNum==j & Data_storage$Scale == "Regional"]<-mean(ses.mntd(samp = reg_data,dis = interspec_mat,null.model = "sample.pool",abundance.weighted = T, runs = 999)$mntd.obs.z)
 
-Data_storage$sesMPDis_abund_z[Data_storage$Dispersal==DispV[i] & Data_storage$ReplicateNum==j & Data_storage$Scale == "Regional"]<-mean(ses.mpd(samp = reg_data,dis = interspec_mat,null.model = "independentswap",abundance.weighted = T, runs = 999)$mpd.obs.z)
+#Data_storage$sesMPDis_abund_z[Data_storage$Dispersal==DispV[i] & Data_storage$ReplicateNum==j & Data_storage$Scale == "Regional"]<-mean(ses.mpd(samp = reg_data,dis = interspec_mat,null.model = "independentswap",abundance.weighted = T, runs = 999)$mpd.obs.z)
 
-Data_storage$sesMNTDis_abund_z[Data_storage$Dispersal==DispV[i] & Data_storage$ReplicateNum==j & Data_storage$Scale == "Regional"]<-mean(ses.mntd(samp = reg_data,dis = interspec_mat,null.model = "independentswap",abundance.weighted = T, runs = 999)$mntd.obs.z)
+#Data_storage$sesMNTDis_abund_z[Data_storage$Dispersal==DispV[i] & Data_storage$ReplicateNum==j & Data_storage$Scale == "Regional"]<-mean(ses.mntd(samp = reg_data,dis = interspec_mat,null.model = "independentswap",abundance.weighted = T, runs = 999)$mntd.obs.z)
 
 Data_storage$sesMPDts_abund_z[Data_storage$Dispersal==DispV[i] & Data_storage$ReplicateNum==j & Data_storage$Scale == "Regional"]<-mean(ses.mpd(samp = reg_data,dis = interspec_mat,null.model = "trialswap",abundance.weighted = T, runs = 999)$mpd.obs.z)
 
@@ -107,6 +107,27 @@ Data_storage$sesMNTDts_abund_z[Data_storage$Dispersal==DispV[i] & Data_storage$R
 
   }
   }
+  
+  require(dplyr) 
+
+Data_storage_total<-summarise(group_by(Data_storage, Dispersal, Scale), Mean_MNTD_abund=mean(MNTD_abund,na.rm=T), SD_MNTD_abund=sd(MNTD_abund,na.rm=T),Mean_MPD_abund=mean(MPD_abund,na.rm=T), SD_MPD_abund=sd(MPD_abund,na.rm=T),
+
+
+
+Mean_sesMNTDpp_abund_z = mean(sesMNTDpp_abund_z,na.rm=T), SD_sesMNTDpp_abund_z = sd(sesMNTDpp_abund_z,na.rm=T), Mean_sesMPDpp_abund_z = mean(sesMPDpp_abund_z,na.rm=T), SD_sesMPDpp_abund_z = sd(sesMPDpp_abund_z,na.rm=T),Mean_sesMNTDtl_abund_z = mean(sesMNTDtl_abund_z,na.rm=T), SD_sesMNTDtl_abund_z = sd(sesMNTDtl_abund_z,na.rm=T), Mean_sesMPDtl_abund_z = mean(sesMPDtl_abund_z,na.rm=T), SD_sesMPDtl_abund_z = sd(sesMPDtl_abund_z,na.rm=T),Mean_sesMNTDr_abund_z = mean(sesMNTDr_abund_z,na.rm=T), SD_sesMNTDr_abund_z = sd(sesMNTDr_abund_z,na.rm=T), Mean_sesMPDr_abund_z = mean(sesMPDr_abund_z,na.rm=T), SD_sesMPDr_abund_z = sd(sesMPDr_abund_z,na.rm=T),Mean_sesMNTDf_abund_z = mean(sesMNTDf_abund_z,na.rm=T), SD_sesMNTDf_abund_z = sd(sesMNTDf_abund_z,na.rm=T), Mean_sesMPDf_abund_z = mean(sesMPDf_abund_z,na.rm=T), SD_sesMPDf_abund_z = sd(sesMPDf_abund_z,na.rm=T),Mean_sesMNTDsp_abund_z = mean(sesMNTDsp_abund_z,na.rm=T), SD_sesMNTDsp_abund_z = sd(sesMNTDsp_abund_z,na.rm=T), Mean_sesMPDsp_abund_z = mean(sesMPDsp_abund_z,na.rm=T), SD_sesMPDsp_abund_z = sd(sesMPDsp_abund_z,na.rm=T),Mean_sesMNTDts_abund_z = mean(sesMNTDts_abund_z,na.rm=T), SD_sesMNTDts_abund_z = sd(sesMNTDts_abund_z,na.rm=T), Mean_sesMPDts_abund_z = mean(sesMPDts_abund_z,na.rm=T), SD_sesMPDts_abund_z = sd(sesMPDts_abund_z,na.rm=T))
+
+ggplot(Data_storage_total,aes(x=Mean_MPD_abund,y=Mean_sesMPDpp_abund_z,color=Scale,group=Scale,fill=Scale,alpha=0.1))+
+
+  geom_line(size=2)+ #plots data as lines
+
+  geom_ribbon(aes(ymin=Mean_sesMPDpp_abund_z-SD_sesMPDpp_abund_z,ymax=Mean_sesMPDpp_abund_z+SD_sesMPDpp_abund_z),width=0.1)+
+
+ 
+
+  theme_bw(base_size = 18)+ #gets rid of grey background
+
+  theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank()) #removes grid lines
+
   
 
 
